f70c7d5593a8de80c925b48fff7ff09dda50c286
Andrea Creazzi <andreacreazzi@libero.it>
2023-08-31 16:43:04 +0200

    add admin.show (complete)

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -6,6 +6,17 @@
             Progetto
         </h2>
         <div class="row justify-content-center">
-
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <a class="text-decoration-none text-dark" href="{{ $projects->link }}">{{ $projects->link }}</a>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $projects->title }}</h5>
+                        <p class="card-text">{{ $projects->description }}</p>
+                        <a href="{{ route('admin.admin.index') }}" class="btn btn-primary">Torna indietro</a>
+                    </div>
+                </div>
+            </div>
         </div>
     @endsection
